7f80cba3bd9cd59644fe0b435f4e158d9330d74d
Suryakant <suryakant.sharma@fatbit.in>
2018-09-28 17:37:13 +0530

    Initial Commit

---
+++ ReusableModules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReusableModules
+//
+//  Created by Mac mini on 9/28/18.
+//  Copyright © 2018 Mac mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
